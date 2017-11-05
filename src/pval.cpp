7961e3162c5d54eebace4996f9e2b49ffecac52c
Ashok Ragavendran <ashok.ragavendran@gmail.com>
2017-11-05 15:15:39 -0500

    still fixing

--- src/pval.cpp
+++ src/pval.cpp
@@ -147,9 +147,11 @@ double compute_lambda_ts(Raw *raw, Sub *sub, unsigned int ncol, double *err_mat,
   // Now fix the ones where subs occurred
   for(s=0;s<sub->nsubs;s++) {
     pos0 = sub->pos[s];
-    if(pos0 < 0 || pos0 >= sub->len0) { Rcpp::stop("CL: Bad pos0: %i (len0=%i).", pos0, sub->len0); }
+    std:string res3 = "CL: Bad pos0:" + std::to_string(pos0)+ "(len0=" + std::to_string(sub->len0)+".";
+    if(pos0 < 0 || pos0 >= sub->len0) { Rcpp::stop(res3); }
     pos1 = sub->map[sub->pos[s]];
-    if(pos1 < 0 || pos1 >= len1) { Rcpp::stop("CL: Bad pos1: %i (len1=%i).", pos1, len1); }
+    std:string res2 = "CL: Bad pos1:" + std::to_string(pos1)+ "(len1=" + std::to_string(len1)+".";
+    if(pos1 < 0 || pos1 >= len1) { Rcpp::stop(res2); }
     
     nti0 = ((int) sub->nt0[s]) - 1;
     nti1 = ((int) sub->nt1[s]) - 1;
